330c59081ad9f59ae9ac7d0166f17278ae110041
zhangke200377 <zhangke200377@outlook.com>
2022-01-25 23:04:02 +0800

    update20220125 歌词部分

--- FPlayer.js
+++ FPlayer.js
@@ -12,7 +12,7 @@ this.fangkehou = this.fangkehou || {};
      *
      * @todo 参数的定义
      *
-     * 这是FPlayer构造函数，实现了FPlayer与容器和FPlayer设置参数的绑定，同时也是FPlayer的类定义。
+     * 这是FPlayer构造函数，实现了FPlayer与容器和FPlayer设置参数的绑定，同时也承载FPlayer的类定义。
      *
      * @class FPlayer
      * @constructor
@@ -146,17 +146,91 @@ this.fangkehou = this.fangkehou || {};
         this.mListener = listener;
     }
 
+    //文档在FPlayer中。。。。。。
     fangkehou.FPlayer = FPlayer;
 
     function Music() {
 
     }
 
+    //文档在Music中。。。。。。
     fangkehou.Music = Music;
 
-    function Lyrics() {
 
+    /**
+     * Lyric 歌词类
+     *
+     * 主要功能为表示和解析歌词，每一个Lyric实例是一行歌词。
+     *
+     * 一般情况下Lyric类会以 Array<Lyric> 的形式出现，Lyric类中也有专门的方法将lrc文件（字符串形式）转换成Lyric数组
+     *
+     * 这是Lyric类的构造函数，同时也承载Lyric的类定义。
+     *
+     * @class Lyric
+     * @constructor
+     *
+     * @param {number} time 开始时间
+     * @param {string} lyric 歌词文本
+     *
+     * @return {Lyric} Lyric对象
+     *
+     */
+    function Lyric(time, lyric) {
+        this.time = time;
+        this.lyric = lyric;
     }
 
-    fangkehou.Lyrics = Lyrics;
+    /**
+     * 这段歌词开始的时间
+     * @type {number}
+     */
+    Lyric.time = undefined;
+    /**
+     * 这段歌词的文本
+     * @type {string}
+     */
+    Lyric.lyric = undefined;
+
+    /**
+     * 可能是Lyric类中唯一的函数了。。。。。负责解析lrc文件（字符串个事）
+     * @param {string} lrcString
+     * @returns {fangkehou.Lyric[]}
+     */
+    Lyric.fromLrcString = function(lrcString){
+        let lyricLines = lrcString.split('\n');
+        let lyricsList = [];
+        let offset = 0;
+        debugger;
+        for(let i = 0; i < lyricLines.length; i++){
+            let currentLyric = lyricLines[i].replace(/[\s]*\[/g, "[").replace(/][\s]*/g, "]");
+            let time = currentLyric.substring(currentLyric.indexOf("[") + 1, currentLyric.indexOf("]")).split(':');
+            if(time[0].toLowerCase() == "offset"){
+                offset = parseInt(time[1]);
+                continue;
+            }
+            if(isNaN(parseInt(time[0]))){
+                continue;
+            }
+            let reg = /\[[0-9:.]*]/g;
+            time = currentLyric.match(reg);
+            let lyricStart = 0;
+            for(let x = 0; x < time.length; x++){
+                lyricStart += time[x].length;
+            }
+            let lyricString = currentLyric.substring(lyricStart);
+            for(let x = 0; x < time.length; x++){
+                let lyricTime = time[x].substring(1, time[x].length);
+                lyricTime = lyricTime.split(':');
+                lyricTime = parseFloat(Math.max((parseInt(lyricTime[0]) * 60 + parseFloat(lyricTime[1]) + offset / 1000.0), 0.0).toFixed(3));
+                lyricsList[lyricsList.length] = new fangkehou.Lyric(lyricTime, lyricString);
+            }
+        }
+        lyricsList.sort((a, b) => {
+            return a.time - b.time;
+        })
+        return lyricsList;
+    }
+
+    //文档在Lyric中。。。。。。
+    fangkehou.Lyric = Lyric;
 }())
